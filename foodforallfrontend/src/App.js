2fec9fa7e82cb0ea2fa488bd9ac220e62a992a30
AdeebIsmail <adeebi5412@gmail.com>
2020-08-15 16:41:07 -0500

    login and signup

--- foodforallfrontend/src/App.js
+++ foodforallfrontend/src/App.js
@@ -5,7 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/navbar.component.js';
 import home from './pages/home.component.js';
 import finder from './pages/finder.component.js';
-import loginuser from './pages/loginuser.component.js';
+import login from './pages/loginuser.component.js';
+import loginbank from './pages/loginbank.component.js'
+import signupbank from './pages/signupbank.component.js'
 import signup from './pages/signupuser.component.js';
 import donate from './pages/donate.component.js';
 
@@ -17,15 +19,10 @@ function App() {
       <Route path='/' exact component={home} />
       <Route path='/donate' exact component={donate} />
       <Route path='/finder' exact component={finder} />
-<<<<<<< HEAD
       <Route path='/userlogin' exact component={login} />
       <Route path='/usersignup' exact component={signup} />
       <Route path='/banklogin' exact component={loginbank} />
       <Route path='/banksignup' exact component={signupbank} />
-=======
-      <Route path='/loginuser' exact component={loginuser} />
-      <Route path='/signup' exact component={signup} />
->>>>>>> 34713d5de6aa4d61f58504187b40bd5d43b9b1c7
       
     </Router>
   );
