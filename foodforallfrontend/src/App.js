53e2047720dd303bc90f4ccf828a5e4e3c4cfb6f
AdeebIsmail <adeebi5412@gmail.com>
2020-08-15 12:54:24 -0500

    login

--- foodforallfrontend/src/App.js
+++ foodforallfrontend/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App"></div>
+    </Router>
   );
 }
 
