d4b75e0527c671737feca18e31335f94b35b20c8
LeftClik <samrut.gadde@gmail.com>
2020-08-15 15:54:19 -0500

    nav

--- foodforallfrontend/src/components/navbar.component.js
+++ foodforallfrontend/src/components/navbar.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
 
@@ -7,17 +8,25 @@ export default class navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className = "navbar-brand">Our Mission</Link>
+                <a href="/" class = "navbar-brand mb-0 h4">FoodForAll</a>
                 <div className="collapse navbar-collapse">
                 <ul className="navbar-nav mr-auto">
                     <li className="navbar-item">
                         <Link to="/Donate" className="nav-link">Donate</Link>
                     </li>
-                    <li className="navbar-item">
-                        <Link to="/Login" className="nav-link">Login</Link>
+                    <li className="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Login</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a href='/userlogin' class="dropdown-item">User Login</a>
+                            <a href="/banklogin" class="dropdown-item">Food Bank Login</a>
+                        </div>
                     </li>
-                    <li className="navbar-item">
-                        <Link to="/Signup" className="nav-link">Sign Up</Link>
+                    <li className="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sign Up</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a href='/usersignup' class="dropdown-item">User Sign Up</a>
+                            <a href="/banksignup" class="dropdown-item">Food Bank Sign Up</a>
+                        </div>
                     </li>
                 </ul>
                 </div>
@@ -25,5 +34,3 @@ export default class navbar extends Component {
         )
     }
 }
-
-//export default navbar
