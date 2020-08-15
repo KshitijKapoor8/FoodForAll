0a7b4389eead3066bf8bf6ed0c19164eff9947a5
AdeebIsmail <adeebi5412@gmail.com>
2020-08-15 15:38:42 -0500

    login and signup

--- foodforallfrontend/src/App.js
+++ foodforallfrontend/src/App.js
@@ -5,9 +5,10 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/navbar.component';
 import home from './pages/home.component';
 import finder from './pages/finder.component';
-//import login from './pages/login.component';
-//import signup from './pages/signup.component';
+import login from './pages/loginuser.component';
+import signup from './pages/signup.component';
 import donate from './pages/donate.component';
+import company from './pages/logincompany.component';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
       <Route path='/' exact component={home} />
       <Route path='/donate' exact component={donate} />
       <Route path='/finder' exact component={finder} />
+      <Route path='/login' exact component={login} />
+      <Route path='/logincompany' exact component={company} />
+      <Route path='/signup' exact component={signup} />
       
     </Router>
   );
