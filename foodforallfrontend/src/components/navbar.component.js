191ce45141606e4a54d125ce0a7318b6c6822909
LeftClik <samrut.gadde@gmail.com>
2020-08-15 16:53:00 -0500

    navbar overhaul

--- foodforallfrontend/src/components/navbar.component.js
+++ foodforallfrontend/src/components/navbar.component.js
@@ -7,26 +7,50 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 export default class navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <a href="/" class = "navbar-brand mb-0 h4">FoodForAll</a>
-                <ul className="nav navbar-nav navbar-right">
+            <nav className="navbar navbar-light bg-light navbar-expand-lg">
+                
+                <ul className="nav navbar-nav">
                     <li className="navbar-item">
-                        <Link to="/Donate" className="nav-link">Donate</Link>
+                        <Link to="/" class = "nav-link h2">FoodForAll</Link>
                     </li>
+
+                    <li className="navbar-item">
+                        <a class = "nav-link h2"></a>
+                    </li>
+                </ul>
+
+                <ul className="nav navbar-nav">
+                    <li className="navbar-item">
+                        <Link to="/Donate" className="nav-link h4">Donate</Link>
+                    </li>
+
+                    <li className="navbar-item">
+                        <Link to="/Finder" className="nav-link h4">Finder</Link>
+                    </li>
+                </ul>
+                <ul className="nav navbar-nav ml-auto">
                     <li className="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Login</a>
+                        <a class="nav-link dropdown-toggle h4" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <span class="glyphicon glyphicon-user"></span> Login</a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a href='/userlogin' class="dropdown-item">User Login</a>
                             <a href="/banklogin" class="dropdown-item">Food Bank Login</a>
                         </div>
                     </li>
                     <li className="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sign Up</a>
+                        <a class="nav-link dropdown-toggle h4" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sign Up</a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a href='/usersignup' class="dropdown-item">User Sign Up</a>
                             <a href="/banksignup" class="dropdown-item">Food Bank Sign Up</a>
                         </div>
-                    </li>    
+                    </li>  
+
+                    <li className="navbar-item">
+                        <a class = "nav-link h2"></a>
+                    </li>  
+
+                    <li className="navbar-item">
+                        <a class = "nav-link h2"></a>
+                    </li>  
                 </ul>
             </nav>
         )
