80192a1f57b56acb6b19064fa7fa9adaff98c72e
AdeebIsmail <adeebi5412@gmail.com>
2020-08-15 22:50:40 -0500

    map

--- foodforallfrontend/src/pages/map.component.tsx
+++ foodforallfrontend/src/pages/map.component.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import Marker from "./Marker";
+import {Map, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
+
+
 
 //const AnyReactComponent = ({text}: any) => <div>{text}</div>;
 // Texas, New Mexico, Washington, Oaklahoma, Arizona, Idaho, Colorado, California
@@ -14,22 +17,22 @@ const SimpleMap = (props: any) => {
         center={{ lat: 29.749907, lng: -95.358421 }}
         defaultZoom={zoom}
       > 
-        <Marker lat={29.780670} lng={-95.274530} text="535 Portwall Street, Houston Food Bank" color="Red" />
-        <Marker lat={29.417040} lng={-98.591660} name="5200 Enrique M Barrera Pkwy, San Antonio Food Bank" color="Red" />
-        <Marker lat={33.003270} lng={-96.760090} text="3677 Mapleshade Lane, Dallas Food Bank" color="Red" />
-        <Marker lat={30.203180} lng={-97.709580} name="6500 Metropolis Drive, Austin Food Bank" color="Red" />
-        <Marker lat={31.670122} lng={ -106.323195} text="9541 Plaza Circle, El Paso Food Bank" color="Red" />
-        <Marker lat={45.580421} lng={122.63169} name="7900 NE 33rd Drive,Oregon Food Bank" color="Red" />
-        <Marker lat={47.170363} lng={-122.444266} text="815 South 96th Street, Seattle Food Bank" color="Red" />
-        <Marker lat={47.659582} lng={ -117.394239} name="1234 E. Front Avenue, Spokane Food Bank" color="Red" />
-        <Marker lat={46.870398} lng={-113.989454} text="5625 Expressway, Montana Food Bank " color="Red" />
-        <Marker lat={43.570399} lng={-116.192014} name="3562 South TK Avenue Idaho Food Bank" color="Red" />
-        <Marker lat={35.144119} lng={-106.601083} text="5840 Office Blvd NE,New Mexico Food Bank" color="Red" />
-        <Marker lat={35.430328} lng={ -97.613708} name="3355 S. Purdue, Oakloahoma Food Bank" color="Red" />
-        <Marker lat={36.170611} lng={-115.098157} text="4190 N. Pecos Rd., Las Vegas Food Bank" color="Red" />
-        <Marker lat={32.903914} lng={ -117.181628} name="9455 Waples Street, Suite 135, San Diego Food Bank" color="Red" />
-        <Marker lat={44.928723} lng={  -93.249358} name="1734 E. 41st Street, Los Angeles Food Bank" color="Red" />
-        <Marker lat={33.409533} lng={  -111.850749} name="245 South Nina Drive, Phoenix Food Bank" color="Red" />
+        <Marker lat={29.780670} lng={-95.274530} title="535 Portwall Street, Houston Food Bank" color="Red"/>   
+        <Marker lat={29.417040} lng={-98.591660} title="5200 Enrique M Barrera Pkwy, San Antonio Food Bank" color="Red" />
+        <Marker lat={33.003270} lng={-96.760090} title="3677 Mapleshade Lane, Dallas Food Bank" color="Red" />
+        <Marker lat={30.203180} lng={-97.709580} title="6500 Metropolis Drive, Austin Food Bank" color="Red" />
+        <Marker lat={31.670122} lng={ -106.323195} title="9541 Plaza Circle, El Paso Food Bank" color="Red" />
+        <Marker lat={45.580421} lng={122.63169} title="7900 NE 33rd Drive,Oregon Food Bank" color="Red" />
+        <Marker lat={47.170363} lng={-122.444266} title="815 South 96th Street, Seattle Food Bank" color="Red" />
+        <Marker lat={47.659582} lng={ -117.394239} title="1234 E. Front Avenue, Spokane Food Bank" color="Red" />
+        <Marker lat={46.870398} lng={-113.989454} title="5625 Expressway, Montana Food Bank " color="Red" />
+        <Marker lat={43.570399} lng={-116.192014} title="3562 South TK Avenue Idaho Food Bank" color="Red" />
+        <Marker lat={35.144119} lng={-106.601083} title="5840 Office Blvd NE,New Mexico Food Bank" color="Red" />
+        <Marker lat={35.430328} lng={ -97.613708} title="3355 S. Purdue, Oakloahoma Food Bank" color="Red" />
+        <Marker lat={36.170611} lng={-115.098157} title="4190 N. Pecos Rd., Las Vegas Food Bank" color="Red" />
+        <Marker lat={32.903914} lng={ -117.181628} title="9455 Waples Street, Suite 135, San Diego Food Bank" color="Red" />
+        <Marker lat={44.928723} lng={  -93.249358} title="1734 E. 41st Street, Los Angeles Food Bank" color="Red" />
+        <Marker lat={33.409533} lng={  -111.850749} title="245 South Nina Drive, Phoenix Food Bank" color="Red" />
       </GoogleMapReact>
     </div>
   );
