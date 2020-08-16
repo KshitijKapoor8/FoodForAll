52da0e0f647c5adfc591edd03fdc410fa29b9fec
AdeebIsmail <adeebi5412@gmail.com>
2020-08-16 00:45:10 -0500

    cards

--- foodforallfrontend/src/pages/home.component.js
+++ foodforallfrontend/src/pages/home.component.js
@@ -1,93 +1,84 @@
-import React from 'react';
-import { Parallax } from 'react-parallax';
-import image from '../hiker.svg'
-import {Card, CardDeck} from 'react-bootstrap'
+import React from "react";
+import { Parallax } from "react-parallax";
+import image from "../hiker.svg";
+import { Card, CardDeck } from "react-bootstrap";
 import "../App.css";
 
-
-
 const pBottomStyle = {
-    background: '#000',
-    height: '1000px',
-    
-}
-
+  background: "#000",
+  height: "1000px",
+};
 
 function home() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <Parallax 
+    <div style={{ textAlign: "center" }}>
+      <Parallax
         bgImage={image}
         strength={200}
-        renderLayer={precentage => (
-          <div 
+        renderLayer={(precentage) => (
+          <div
             style={{
-              position: 'absolute',
-              width: '100px',
-              height: '100px',
-              borderRadius: '50%',
+              position: "absolute",
+              width: "100px",
+              height: "100px",
+              borderRadius: "50%",
               background: `rgba(255, 123, 23, ${precentage * 1})`,
-              left: '50%',
-              top: '50%',
+              left: "50%",
+              top: "50%",
               transform: `translate(-50%, -50%) scale(${precentage * 5})`,
             }}
           >
-            <div style={{
-              position: 'absolute',
-              left: '50%',
-              top: '50%',
-              transform: `translate(-50%, -50%) scale(${precentage * 5})`,
-            }}>FoodForAll</div>
+            <div
+              style={{
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                transform: `translate(-50%, -50%) scale(${precentage * 5})`,
+              }}
+            >
+              FoodForAll
+            </div>
           </div>
-                  
         )}
       >
-        <div style={{ height: 835 }}>
-
-        </div>
+        <div style={{ height: 835 }}></div>
       </Parallax>
-      <div style={{ height: '3vh' }}></div>
-      <div style={{marginTop: 20, marginBottom: 20, width: 900}}>
-        <CardDeck >
-          <Card className="mx-auto">
-            <Card.Img variant="top" src="https://picsum.photos/100/100" />
+      <div style={{ height: "3vh" }}></div>
+        <CardDeck>
+          <Card className="mx-auto; " border="success" style={{ width: '18rem' }}>
+            <Card.Img variant="top" />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
               </Card.Text>
             </Card.Body>
           </Card>
-          <Card>
-            <Card.Img variant="top" src="https://picsum.photos/100/100" />
+          <Card border="success" style={{ width: '18rem' }}>
+            <Card.Img variant="top" />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
-                This card has supporting text below as a natural lead-in to additional
-                content.{' '}
+                This card has supporting text below as a natural lead-in to
+                additional content.{" "}
               </Card.Text>
             </Card.Body>
           </Card>
-          <Card>
-            <Card.Img variant="top" src="https://picsum.photos/100/100" />
+          <Card border="success" style={{ width: '18rem' }}>
+            <Card.Img variant="top" />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This card has even longer content than the first to
-                show that equal height action.
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
               </Card.Text>
             </Card.Body>
           </Card>
         </CardDeck>
-        </div>
-      
-
-
-    </div>
-
-    
+      </div>
   );
 }
 
